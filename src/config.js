3a8a10de14c33c4ced628f27404d2a860870d305
Andrew Thomas <andrewmthomas87@gmail.com>
2018-05-27 21:13:12 -0500

    Spotify authentication

---
+++ src/config.js
@@ -0,0 +1,8 @@
+
+const config = {
+	clientId: '6ad25173269b4eff932147cd82c9fc7b',
+	clientSecret: '93dcd52ae75b48709a82c34151d37e48',
+	redirectUri: 'http://localhost:7777/spotify-redirect'
+}
+
+module.exports = config
--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 const express = require('express')
+const fetch = require('node-fetch')
 
+const config = require('./config')
 const connection = require('./connection')
 
 const server = express()
@@ -8,6 +10,34 @@ server.use(express.json())
 
 server.get('/', (request, response) => {
 	response.send('Hello world')
+})
+
+server.get('/spotify-sign-in', (request, response) => {
+	response.redirect(`https://accounts.spotify.com/authorize/?client_id=${config.clientId}&response_type=code&redirect_uri=${config.redirectUri}`)
+})
+
+server.get('/spotify-redirect', (request, response) => {
+	const code = request.query.code
+	if (!code) {
+		response.sendStatus(400)
+		return
+	}
+
+	const authorization = `${config.clientId}:${config.clientSecret}`
+	fetch('https://accounts.spotify.com/api/token', {
+		method: 'POST',
+		headers: {
+			'Authorization': `Basic ${Buffer.from(authorization).toString('base64')}`,
+			'Content-Type': 'application/x-www-form-urlencoded'
+		},
+		body: `grant_type=authorization_code&code=${code}&redirect_uri=${config.redirectUri}`
+	}).then((response) => {
+		return response.json()
+	}).then((data) => {
+		response.send(JSON.stringify(data))
+	}).catch(() => {
+		response.sendStatus(500)
+	})
 })
 
 server.get('/sessions', (request, response) => {
